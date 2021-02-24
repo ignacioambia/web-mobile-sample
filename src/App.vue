a3a89afde63c46c872d0f069e68295b59c92f841
Ignacio <ignacio.ambia@gmail.com>
2021-02-23 20:16:14 -0600

    change font family

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,17 @@
 </script>
 
 <style lang="scss">
+
+@font-face {
+    font-family: "Avenir";
+    src : url('assets/fonts/avenir_heavy.ttf') format("truetype");
+}
+
   body{
     background-color: $solid-black;
     padding : 0px;
     margin : 0;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: "Avenir", Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color : white;
